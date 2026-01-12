ccd202e3e83d2d929efde0bf68aec665ca10ce16
Sergey Ukolov <zezic51@yandex.ru>
2026-01-12 08:16:32 +0300

    Update docs

---
+++ _nuxt/static/1768194780/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1768194780",layout:"default",error:a,state:{counter:0,i18n:{language:"en"}},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
